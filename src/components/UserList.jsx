3a494af9ac6def7b77a296618d3d55bb6159fc21
Azuga99 <90208886+Azuga99@users.noreply.github.com>
2022-03-17 22:13:39 +0200

    second commit

---
+++ src/components/UserList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import UserItem from "./UserItem";
+
+function UserList(props){
+    console.log(props);
+    const {users} = props;
+    return(
+        <div>
+            {
+                users.map((user, index) => {
+                    return  <UserItem
+                        name={user.name}
+                        email={user.email}
+                        isGoldClient={user.isGoldClient}
+                        key={index}
+                    />
+                } )
+
+            }
+        </div>
+    );
+}
+
+export default UserList;
